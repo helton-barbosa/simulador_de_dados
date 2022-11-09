206c60724b2e68066065dfacdf67084b9b10ddfb
Helton Barbosa <heltonbs@gmail.com>
2022-11-08 21:56:02 -0300

    Refatorando
    Algumas modificações

--- simulador_de_dados.py
+++ simulador_de_dados.py
@@ -7,17 +7,17 @@ class SimuladorDeDado:
         self.janela = None
         self.eventos = None
         self.valores = None
-        self.valor_minimo = 1
-        self.valor_maximo = 6
+        self.menor_face_do_dado = 1
+        self.maior_face_do_dado = 6
         # Layout
-        self.layout = [
+        self.caixa = [
             [sg.Text('Jogar o dado?')],
-            [sg.Button('Sim'), sg.Button('Não')]
+            [sg.Button('Sim'), sg.Button('Não'), sg.Button('Sair')]
         ]
 
     def iniciar(self):
         # Criar uma janela
-        self.janela = sg.Window('Simulador de Dado', layout=self.layout)
+        self.janela = sg.Window('Simulador de Dados', layout=self.caixa)
         # Ler os valores da tela
         self.eventos, self.valores = self.janela.Read()
         # Fazer alguma coisa com esses valores
@@ -27,12 +27,12 @@ class SimuladorDeDado:
             elif self.eventos == 'Não':
                 print('Agradecemos sua participação!')
             else:
-                print('Favor digitar sim ou não.')
+                print('Saindo do programa!')
         except:
             print('Ocorreu um erro ao receber sua resposta.')
 
     def gerar_valor_do_dado(self):
-        print(random.randint(self.valor_minimo, self.valor_maximo))
+        print(f'Foi sorteado a face {random.randint(self.menor_face_do_dado, self.maior_face_do_dado)} do dado.')
 
 
 simulador = SimuladorDeDado()
